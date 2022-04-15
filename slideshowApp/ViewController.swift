46e4288306da8b4b1772048ace072b2b72380004
tetA <doublepotato-temporarygittempo@yahoo.co.jp>
2022-04-15 16:57:34 +0900

    課題要件について上から「最初の画像の表示時に、戻るボタンをタップすると、最後の画像が表示されるようにしてください」まで進んだ状態。

--- slideshowApp/ViewController.swift
+++ slideshowApp/ViewController.swift
@@ -8,12 +8,59 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let pics = ["Image-1", "Image-2", "Image-3"]
+    var picsnum = 0
 
+    
+    @IBOutlet weak var imagepic: UIImageView!
+    
+    // 進むボタンをタップして次の画像を表示する
+    @IBAction func nextButton(_ sender: Any) {
+        if pics[picsnum] == pics.last {
+            
+            picsnum = 0
+            
+            let name = pics[picsnum]
+            imagepic.image = UIImage(named: name)
+            
+        } else {
+            
+            picsnum += 1
+            
+            let name = pics[picsnum]
+            imagepic.image = UIImage(named: name)
+        }
+    }
+    
+    //戻るボタンをタップして前の画像を表示する
+    @IBAction func backButton(_ sender: Any) {
+        if pics[picsnum] == pics.first {
+            
+            picsnum = pics.endIndex - 1
+            
+            let name = pics[picsnum]
+            imagepic.image = UIImage(named: name)
+            
+        } else {
+            
+            picsnum -= 1
+            
+            let name = pics[picsnum]
+            imagepic.image = UIImage(named: name)
+        }
+    }
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
-
+    
+    
 }
 
